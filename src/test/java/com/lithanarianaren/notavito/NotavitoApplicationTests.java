bc3eaa82f216323574bdf5657c0b63ae037b121f
Lithanarianaren <murenky@yandex.ru>
2025-07-01 22:16:32 +0300

    Initial commit

---
+++ src/test/java/com/lithanarianaren/notavito/NotavitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lithanarianaren.notavito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotavitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
